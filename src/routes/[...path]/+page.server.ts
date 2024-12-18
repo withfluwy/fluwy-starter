1c6c2af48b33de3f8f4c3bd38bfc5e4b16401b93
Marco Avila <marcotulio.avila@gmail.com>
2024-12-17 20:12:52 -0800

    add plugin format for the app with welcome component

--- src/routes/[...path]/+page.server.ts
+++ src/routes/[...path]/+page.server.ts
@@ -1,14 +1,6 @@
-import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { app } from '@fluwy/ui';
 
 export const load: PageServerLoad = async ({ params, cookies }) => {
-	app.config = {
-		pages: 'app/pages',
-		layouts: 'app/layouts',
-		themes: 'app/themes',
-		error,
-		redirect
-	};
 	return app.render(params.path, { auth_token: cookies.get('auth_token') });
 };
